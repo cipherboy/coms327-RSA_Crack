76c36eb60b7d9b611d311d3534cfa1610009b621
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-04-27 16:25:36 -0500

    Clean up rsa-crack.c

--- rsa-crack.c
+++ rsa-crack.c
@@ -1,50 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <gmp.h>
-#include <sys/time.h>
-#include <string.h>
-#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
-    FILE *pt, *ct;
-    FILE *pub, *prv;
-    char *string;
-    int i, bytes, c;
+    FILE *pub;
+    int i;
     int bits = 4096;
-    char ch;
 
-    mpz_t n, pm1, e, p, q, pm2, pm3;
+    mpz_t n, p, q, pm1, pm2, pm3;
+
+    unsigned int seed = atoi(argv[1]);
+    unsigned int scount = atoi(argv[2]);
+    unsigned int loops = 0;
 
     mpz_init(n);
-    mpz_init(e);
+    mpz_init(p);
+    mpz_init(q);
+    mpz_init(pm1);
     mpz_init(pm2);
     mpz_init(pm3);
 
     pub = fopen("gmp2.pub", "r");
     mpz_inp_raw(n, pub);
-    mpz_inp_raw(e, pub);
 
     gmp_randstate_t state;
-    FILE *out;
-    struct timeval tv;
-
-    mpz_set_ui(e, 65537);
 
     gmp_randinit_mt(state);
-    gettimeofday(&tv, NULL);
-
-    unsigned int seed = atoi(argv[1]);
-    unsigned int scount = atoi(argv[2]);
-
-    unsigned int loops = 0;
 
     int j = 0;
-
-    mpz_init(p);
-    mpz_init(q);
-    mpz_init(pm1);
 
     for (; loops < scount; loops++) {
         gmp_randseed_ui(state,  seed+loops);
@@ -54,28 +38,33 @@ int main(int argc, char *argv[])
 
         mpz_mul(pm1, p, q);
         mpz_set(pm2, pm1);
-        mpz_mul_ui(pm3, p, 66000);
-        mpz_add(pm2, pm2, pm3);
-        mpz_mul_ui(pm3, q, 66000);
-        mpz_add(pm2, pm2, pm3);
-        mpz_mul_ui(pm3, e, 66000);
+        // a = 1,600,000,000
+        mpz_mul_ui(pm3, p, 1600000000);
         mpz_add(pm2, pm2, pm3);
 
+        // p' = p + c
+        // q' = q + d
+        //
+        // pq <= n <= pq + ap
+        //
+        // Where a is a nice large constant such that
+        // pq <= n <= pq + pd + qc + cd <=  pq + ap
         if (mpz_cmp(n, pm1) >= 0) {
             if (mpz_cmp(pm2, n) >= 0) {
                 printf("Found seed candidate: %u\n", seed+loops);
                 gmp_printf("%Zd\n", p);
+
+                // Not sure if only candidate so don't return
             }
         }
     }
+    mpz_clear(n);
     mpz_clear(p);
     mpz_clear(q);
     mpz_clear(pm1);
     mpz_clear(pm2);
     mpz_clear(pm3);
     gmp_randclear(state);
-    mpz_clear(n);
-    mpz_clear(e);
 
-    return 2;
+    return 0;
 }
